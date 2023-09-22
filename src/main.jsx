862d71e94efbd7b91755538a4051bce8a578af5a
mdabarik <mdabarik19@gmail.com>
2023-09-23 02:26:26 +0600

    some components created

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Root from './components/Root/Root'
+import Home from './components/Home/Home'
+import AppliedJobs from './components/AppliedJobs/AppliedJobs'
+import ErrorPage from './components/ErrorPage/ErrorPage'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/applied',
+        element: <AppliedJobs></AppliedJobs>
+      },
+      {
+        path: '/blogs',
+        element: 'blogs'
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
